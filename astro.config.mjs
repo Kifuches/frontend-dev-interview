bd527382c519a066a9179170fac260d5ff9aa8ff
Граева Анастасия <kifuches@yandex.ru>
2026-06-24 14:34:01 +0300

    созранение скрола в сайдбаре. темы для блоков с кодом

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,13 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://kifuches.github.io',
   base: '/frontend-dev-interview',
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      defaultColor: false,
+    },
+  },
 });
